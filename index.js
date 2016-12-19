9bc419a112f57565ecfd7ed5a22517306ba75c8c
misund <thomas@hemmeligadresse.com>
2016-12-19 17:37:43 +0100

    🔰 Append/remove trailing slashes
    This is basically a javsacript port of WordPress' trailingslashit(). Here's the original:
    https://github.com/WordPress/WordPress/blob/c7988f1f0311a8a3eadfe9a78c0c82971822d728/wp-includes/formatting.php#L2357-L2388
    
    Thanks for sharing, WordPress!

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Removes trailing forward slashes if they exist.
+ *
+ * If the string doesn't end with a slash, we simply return it.
+ */
+const unTrailingSlashIt = (str) => {
+  if (str.endsWith('/')) {
+    return unTrailingSlashIt(str.slice(0, -1));
+  }
+
+  return str;
+};
+
+/**
+ * Appends a trailing slash.
+ *
+ * Will remove a trailing forward slash if it exists already, before adding a
+ * trailing forward slash. This prevents double slashing a string or path.
+ */
+const trailingSlashIt = (str) => {
+  return unTrailingSlashIt(str) + '/';
+};
+
+export {
+  trailingSlashIt,
+  unTrailingSlashIt,
+};
+
+export default trailingSlashIt;
