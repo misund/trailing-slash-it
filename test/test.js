2f018d0618797b0f2df18801bdc5b8378bcf8770
Just Thomas Hiorth Misund <thomas@hemmeligadresse.com>
2017-02-21 03:22:57 +0100

    Feature/backslashes (#2)
    * Remove backslashes as well as forward slashes
    
    * 0.3.0

--- test/test.js
+++ test/test.js
@@ -6,11 +6,13 @@ import {
 } from '..';
 
 describe('trailingSlashIt as default export', function() {
-  describe('add a slash', function() {
+  describe('naked string', function() {
     it('should add a slash to a string', function() {
       assert.equal('foo/', trailingSlashIt('foo'))
     });
+  });
 
+  describe('forward slashes', function() {
     it('should remove a single trailing slash before adding one', function() {
       assert.equal('foo/', trailingSlashIt('foo/'));
     });
@@ -23,34 +25,66 @@ describe('trailingSlashIt as default export', function() {
       assert.equal('foo/', trailingSlashIt('foo///'));
     });
   });
+
+  describe('backslashes', function() {
+    it('should remove a single trailing backslash before adding a forward slash', function() {
+      assert.equal('foo/', trailingSlashIt('foo\\'));
+    });
+
+    it('should remove two trailing backslashes before adding a forward slash', function() {
+      assert.equal('foo/', trailingSlashIt('foo\\\\'));
+    });
+
+    it('should remove alternating trailing forward and backslashes before adding a forward slash', function() {
+      assert.equal('foo/', trailingSlashIt('foo\\/\\//'));
+    });
+  });
 });
 
 describe('trailingSlashIt as non-default export', function() {
-  describe('add a slash', function() {
+  describe('naked string', function() {
     it('should add a slash to a string', function() {
-      assert.equal('foo/', tsi('foo'))
+      assert.equal('foo/', trailingSlashIt('foo'))
     });
+  });
 
+  describe('forward slashes', function() {
     it('should remove a single trailing slash before adding one', function() {
-      assert.equal('foo/', tsi('foo/'));
+      assert.equal('foo/', trailingSlashIt('foo/'));
     });
 
     it('should remove two trailing slashes before adding one', function() {
-      assert.equal('foo/', tsi('foo//'));
+      assert.equal('foo/', trailingSlashIt('foo//'));
     });
 
     it('should remove several trailing slashes before adding one', function() {
-      assert.equal('foo/', tsi('foo///'));
+      assert.equal('foo/', trailingSlashIt('foo///'));
+    });
+  });
+
+  describe('backslashes', function() {
+    it('should remove a single trailing backslash before adding a forward slash', function() {
+      assert.equal('foo/', trailingSlashIt('foo\\'));
+    });
+
+    it('should remove two trailing backslashes before adding a forward slash', function() {
+      assert.equal('foo/', trailingSlashIt('foo\\\\'));
+    });
+
+    it('should remove alternating trailing forward and backslashes before adding a forward slash', function() {
+      assert.equal('foo/', trailingSlashIt('foo\\/\\//'));
     });
   });
 });
 
 describe('unTrailingSlashIt as non-default export', function() {
-  describe('remove a slash', function() {
+  describe('naked string', function() {
     it('should not do anything with a string that does not end with a slash', function() {
       assert.equal('foo', utsi('foo'))
     });
+  });
 
+  describe('forward slashes', function() {
     it('should remove a single trailing slash', function() {
       assert.equal('foo', utsi('foo/'));
     });
@@ -61,6 +95,20 @@ describe('unTrailingSlashIt as non-default export', function() {
 
     it('should remove several trailing slashes', function() {
       assert.equal('foo', utsi('foo///'));
+    });
+  });
+
+  describe('backslashes', function() {
+    it('should remove a single trailing backslash', function() {
+      assert.equal('foo', utsi('foo\\'));
+    });
+
+    it('should remove two trailing backslashes', function() {
+      assert.equal('foo', utsi('foo\\'));
+    });
+
+    it('should remove alternating trailing forward and backslashes', function() {
+      assert.equal('foo', utsi('foo\\/\\/'));
     });
   });
 });
